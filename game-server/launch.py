99654937e8b6fa4c2e902464d912b423b370c895
Jacob Schmidt <innovativestudios@outlook.com>
2023-06-09 23:27:43 -0500

    Hotfix 06092023@2330

--- game-server/launch.py
+++ game-server/launch.py
@@ -10,6 +10,10 @@ def mod_param(name, mods):
     return ' -{}= "{}" '.format(name, ";".join(mods))
 
 
+def env_defined(key):
+    return key in os.environ and len(os.environ[key]) > 0
+
+
 CONFIG_FILE = os.environ["ARMA_CONFIG"]
 KEYS = "/arma3/keys"
 
@@ -18,21 +22,30 @@ if not os.path.isdir(KEYS):
         os.remove(KEYS)
     os.makedirs(KEYS)
 
-steamcmd = ["/steamcmd/steamcmd.sh"]
-steamcmd.extend(
-    [
-        "+force_install_dir",
-        "/arma3",
-        "+login",
-        os.environ["STEAM_USERNAME"],
-        os.environ["STEAM_PASSWORD"],
-        "+app_update",
-        "233780",
-        "validate",
-        "+quit",
-    ]
-)
-subprocess.call(steamcmd)
+if os.environ["SKIP_INSTALL"] in ["", "false"]:
+    steamcmd = ["/steamcmd/steamcmd.sh"]
+    steamcmd.extend(
+        [
+            "+force_install_dir",
+            "/arma3",
+            "+login",
+            os.environ["STEAM_USERNAME"],
+            os.environ["STEAM_PASSWORD"],
+            "+app_update",
+            "233780",
+        ]
+    )
+    if env_defined("STEAM_BRANCH"):
+        steamcmd.extend(["-beta", os.environ["STEAM_BRANCH"]])
+    if env_defined("STEAM_BRANCH_PASSWORD"):
+        steamcmd.extend(["-betapassword", os.environ["STEAM_BRANCH_PASSWORD"]])
+    steamcmd.extend(
+        [
+            "validate",
+            "+quit",
+        ]
+    )
+    subprocess.call(steamcmd)
 
 mods = []
 
@@ -50,6 +63,10 @@ launch = "{} -limitFPS={} -world={} {} {}".format(
     mod_param("mod", mods),
 )
 
+if os.environ["ARMA_CDLC"] != "":
+    for cdlc in os.environ["ARMA_CDLC"].split(";"):
+        launch += " -mod={}".format(cdlc)
+
 launch += ' -config="/arma3/configs/{}"'.format(CONFIG_FILE)
 launch += ' -port={} -name="{}" -profiles="/arma3/configs/profiles"'.format(
     os.environ["PORT"], os.environ["ARMA_PROFILE"]
@@ -58,5 +75,5 @@ launch += ' -port={} -name="{}" -profiles="/arma3/configs/profiles"'.format(
 if os.path.exists("servermods"):
     launch += mod_param("serverMod", local.mods("servermods"))
 
-print("Launching Arma Server With", launch, flush=True)
+print("LAUNCHING ARMA SERVER WITH", launch, flush=True)
 os.system(launch)
